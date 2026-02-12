5ed7f138b29303d5466ef89b17fdc09d316e31b9
bird01 <bird01@vecskill.ovec>
2026-02-12 17:21:07 +0700

    test

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const fileupload = require('express-fileupload')
+const path = require('path')
+const cors = require('cors')
+const app = express()
+
+app.use(cors ({
+    origin:'http://localhost:3000',
+    credentials: true
+}))
+
+app.use(express.json())
+
+app.use((req,res) => res.status(404).json({message:'404 : Invalid Route'}))
+app.listen(3001 , () => console.log("Server Running On Port 3001"))
