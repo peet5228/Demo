35e292adc94359b0d7fd842e35a65eda1dac17ac
bird01 <bird01@vecskill.ovec>
2026-02-13 09:36:37 +0700

    pat

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,73 @@
 <template>
-    <slot />
+  <v-app>
+    <v-app-bar color="#7d0c14" flat>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title>NTC evaluation system</v-toolbar-title>
+      <v-spacer />
+      <!-- โปรไฟล์ -->
+       <p class="text-center">ผู้ใช้งาน : {{ user.first_name }} {{ user.last_name }} <br> {{ user.role }}</p>&nbsp;&nbsp;
+      <v-btn icon="mdi-logout" @click="logout" variant="text" />&nbsp;&nbsp;
+    </v-app-bar>
+    
+    <v-navigation-drawer v-model="drawer" width="260" app color="#404040" :temporary="isMobile" :permanent="!isMobile">
+      <v-list density="comfortable" nav>
+        <v-list-item v-for="item in navitem" :key="item.title" :to="item.to">
+            <v-list-item-title>
+                {{ item.title }}
+            </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid class="py-2">
+        <slot />
+      </v-container>
+      <v-footer app class="text-caption justify-center">© 2025 NTC evaluation system</v-footer>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
+import { useDisplay } from 'vuetify';
+import {api} from '../API/base'
+
+const {mdAndDown} = useDisplay()
+const isMobile = computed(() => mdAndDown.value)
+const drawer = ref(false)
+const user = ref<any>({})
+
+const roles = [
+    //staff
+    {title:'หน้าหลัก',to:'/Staff/',role:'ฝ่ายบุคลากร'},
+
+    //commit
+    {title:'รายชื่อผู้รับการประเมิน',to:'/Committee/',role:'กรรมการประเมิน'},
+
+    //eva
+    {title:'หน้าหลัก',to:'/Evaluatee/',role:'ผู้รับการประเมินผล'},
+]
+const navitem = computed(() => roles.filter((item) => item.role.includes(user.value.role)))
+
+const logout = () =>{
+    if(!confirm('ต้องการออกจากระบบใช่หรอไม่?'))return
+    localStorage.removeItem('token')
+    navigateTo('/',{replace:true})
+}
+
+const fetchUser = async () =>{
+    const token = localStorage.getItem('token')
+    if(!token){
+        return await navigateTo('/',{replace:true})
+    }
+    try{
+        const res = await axios.get(`${eva}/profile`,{headers:{Authorization:`Bearer ${token}`}})
+        user.value = res.data
+    }catch(err){
+        console.error('Error Get Profile!',err)
+    }
+}
 
 </script>
 
